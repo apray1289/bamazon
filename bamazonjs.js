bd9ff8010fbf8039a6960f105d11dd5568aa7188
Andrew Pray <a.pray1289@gmail.com>
2019-09-09 22:06:54 -0500

    Query is working

--- bamazonjs.js
+++ bamazonjs.js
@@ -1,186 +1,62 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-
 var connection = mysql.createConnection({
   host: "localhost",
 
   port: 3306,
   user: "root",
 
-  password: "",
+  password: "password",
   database: "bamazon"
 });
 
-
 connection.connect(function(err) {
-    if (err) throw err;
-    runSearch();
+  if (err) throw err;
+  console.log("Connected");
+});
+
+function displayProducts() {
+  connection.query("SELECT * FROM products", function(err, results) {
+    for (var i = 0; i < results.length; i++) {
+      console.log(`------------------------
+    Product Name: ${results[i].product_name} | ID: ${results[i].item_id} | Price: ${results[i].price}`);
+    }
+
+    findProduct();
   });
-  
-  function runSearch() {
-    inquirer
-      .prompt({
+}
+
+displayProducts();
+
+function findProduct() {
+  inquirer
+    .prompt([
+      {
         name: "productId",
         type: "input",
-        message: "What is the ID of the product you want to buy?",
-        
-      })
-      .then(function(answer) {
-        switch (answer.action) {
-        case "Find songs by artist":
-          artistSearch();
-          break;
-  
-        case "Find all artists who appear more than once":
-          multiSearch();
-          break;
-  
-        case "Find data within a specific range":
-          rangeSearch();
-          break;
-  
-        case "Search for a specific song":
-          songSearch();
-          break;
-  
-        case "Find artists with a top song and top album in the same year":
-          songAndAlbumSearch();
-          break;
-        }
-      });
-  }
-  
-  function artistSearch() {
-    inquirer
-      .prompt({
-        name: "artist",
+        message: "What is the ID of the product you want to buy?"
+      },
+
+      {
+        name: "quantity",
         type: "input",
-        message: "What artist would you like to search for?"
-      })
-      .then(function(answer) {
-        var query = "SELECT position, song, year FROM top5000 WHERE ?";
-        connection.query(query, { artist: answer.artist }, function(err, res) {
-          for (var i = 0; i < res.length; i++) {
-            console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-          }
-          runSearch();
-        });
-      });
-  }
-  
-  function multiSearch() {
-    var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-    connection.query(query, function(err, res) {
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].artist);
+        message: "How much of the product do you want?"
       }
-      runSearch();
-    });
-  }
-  
-  function rangeSearch() {
-    inquirer
-      .prompt([
-        {
-          name: "start",
-          type: "input",
-          message: "Enter starting position: ",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
+    ])
+    .then(function(answer) {
+      connection.query(
+        "SELECT * FROM products WHERE ?",
+        [
+          {
+            item_id: answer.productId
           }
-        },
-        {
-          name: "end",
-          type: "input",
-          message: "Enter ending position: ",
-          validate: function(value) {
-            if (isNaN(value) === false) {
-              return true;
-            }
-            return false;
-          }
+        ],
+        function(err, results) {
+
+          console.log(results[0].product_name);
+         
         }
-      ])
-      .then(function(answer) {
-        var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-        connection.query(query, [answer.start, answer.end], function(err, res) {
-          for (var i = 0; i < res.length; i++) {
-            console.log(
-              "Position: " +
-                res[i].position +
-                " || Song: " +
-                res[i].song +
-                " || Artist: " +
-                res[i].artist +
-                " || Year: " +
-                res[i].year
-            );
-          }
-          runSearch();
-        });
-      });
-  }
-  
-  function songSearch() {
-    inquirer
-      .prompt({
-        name: "song",
-        type: "input",
-        message: "What song would you like to look for?"
-      })
-      .then(function(answer) {
-        console.log(answer.song);
-        connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-          console.log(
-            "Position: " +
-              res[0].position +
-              " || Song: " +
-              res[0].song +
-              " || Artist: " +
-              res[0].artist +
-              " || Year: " +
-              res[0].year
-          );
-          runSearch();
-        });
-      });
-  }
-  
-  function songAndAlbumSearch() {
-    inquirer
-      .prompt({
-        name: "artist",
-        type: "input",
-        message: "What artist would you like to search for?"
-      })
-      .then(function(answer) {
-        var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-        query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-        query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-  
-        connection.query(query, [answer.artist, answer.artist], function(err, res) {
-          console.log(res.length + " matches found!");
-          for (var i = 0; i < res.length; i++) {
-            console.log(
-              i+1 + ".) " +
-                "Year: " +
-                res[i].year +
-                " Album Position: " +
-                res[i].position +
-                " || Artist: " +
-                res[i].artist +
-                " || Song: " +
-                res[i].song +
-                " || Album: " +
-                res[i].album
-            );
-          }
-  
-          runSearch();
-        });
-      });
-  }
+      );
+    });
+}
